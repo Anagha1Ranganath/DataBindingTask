414142e353d203c98b69624b49ea229f58b300ab
Anagha <cranagh1@in.ibm.com>
2019-01-31 17:57:50 +0530

    second commit

--- src/app/task2/task2.module.ts
+++ src/app/task2/task2.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Component2Component } from './component2/component2.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [Component2Component],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule
   ],
   exports: [
     Component2Component
